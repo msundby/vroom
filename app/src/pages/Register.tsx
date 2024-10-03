204434271c16893d48f84c64ebdd382f9796aee9
msundby <114078619+msundby@users.noreply.github.com>
2024-10-03 20:01:53 +0200

    Close to done

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet,Image, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image, ScrollView, Alert } from 'react-native';
 import Button from '../components/Button';
 import * as ImagePicker from 'expo-image-picker';
 import { TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Register: React.FC = () => {
 
+  const [user, setUser] = useState({ name: '', address: '', password: '', email: '', driverLicenseNumber: '', phoneNumber: '', profileImagePath: '', licenseImagePath: ''});
   const [profileImg, setProfileImg] = useState<string | null>(null);
   const [driversLicenseImg, setDriversLicenseImg] = useState<string | null>(null);
+
+  if(user == null){
+
+  }
 
   const pickProfileImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -20,6 +26,7 @@ const Register: React.FC = () => {
 
     if (!result.canceled) {
       setProfileImg(result.assets[0].uri);
+      setUser((prevUser) => ({ ...prevUser, profileImagePath: result.assets[0].uri }));
     }
   };
 
@@ -33,6 +40,16 @@ const Register: React.FC = () => {
 
     if (!result.canceled) {
       setDriversLicenseImg(result.assets[0].uri);
+      setUser((prevUser) => ({ ...prevUser, licenseImagePath: result.assets[0].uri }));
+    }
+  };
+
+  const saveUser = async (user: any) => {
+    try {
+      await AsyncStorage.setItem('@user', JSON.stringify(user));
+      Alert.alert('Success', 'User saved successfully!');
+    } catch (error) {
+      console.error('Error saving user:', error);
     }
   };
 
@@ -49,6 +66,7 @@ const Register: React.FC = () => {
           (<Image source={{ uri: profileImg }} style={styles.addedPhoto} />) 
           : 
           (<Image source={require('../../assets/AddProfileImageGold.png')} style={styles.addPhoto} />)
+          
           }
         </TouchableOpacity>
       </View>
@@ -56,60 +74,64 @@ const Register: React.FC = () => {
       <View style={styles.form}>
         <TextInput
           placeholder="Full name"
-            style={styles.input}
-              placeholderTextColor="white"
+          style={styles.input}
+          placeholderTextColor="white"
+          value={user.name}
+          onChangeText={(text) => setUser((prevUser) => ({ ...prevUser, name: text }))}
+            
         />
         <TextInput
           placeholder="Address"
           style={styles.input}
           placeholderTextColor="white"
+          value={user.address}
+          onChangeText={(text) => setUser((prevUser) => ({ ...prevUser, address: text }))}
         />
         <TextInput
           placeholder="Email"
           style={styles.input}
           placeholderTextColor="white"
+          value={user.email}
+          onChangeText={(text) => setUser((prevUser) => ({ ...prevUser, email: text }))}
         />
         <TextInput
           placeholder="Phone Number"
           style={styles.input}
           placeholderTextColor="white"
+          value={user.phoneNumber}
+          onChangeText={(text) => setUser((prevUser) => ({ ...prevUser, phoneNumber: text }))}
         />
         <TextInput
           placeholder="Driver License Number"
           style={styles.input}
           placeholderTextColor="white"
+          value={user.driverLicenseNumber}
+          onChangeText={(text) => setUser((prevUser) => ({ ...prevUser, driverLicenseNumber: text }))}
         />
+        
+        
         <View style={styles.button}>
-      <View style={styles.container}>
-
-      <Button title="Upload Driver's License" onPress={pickDriversLicenseImage} />
-      {driversLicenseImg && <Image source={{ uri: driversLicenseImg }} style={styles.image} />}
-      </View>
+        
+        <Button title="Upload Driver's License" onPress={pickDriversLicenseImage} />
+        <View style={styles.containerImage}>
+          {driversLicenseImg && <Image source={{ uri: driversLicenseImg }} style={styles.image} />}
+          </View>
+        </View>
 
         <View style={styles.button}>
         <Button
-      title="Register"
-      onPress={() => {
-        console.log('Button pressed');
+          title="Register"
+          onPress={() => {
+            console.log(user)
       }} />
       </View>
       
       </View>
-      </View>
+      
       
     </ScrollView>
   );
 };
-
-
-
-
-
-
-
-
-
-
 
 
 const styles = StyleSheet.create({
@@ -119,7 +141,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 50,
     backgroundColor: '#2c2c2e'
-    
+  },
+  containerImage: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5
   },
   backgroundContainer: {
     width: '100%',
@@ -155,10 +182,12 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: 10,
     marginBottom: 2,
-    backgroundColor: '#3a3a3c'
+    backgroundColor: '#3a3a3c',
+    color: 'white'
   },
   button: {
-    marginTop: 20
+    marginTop: 5,
+    marginBottom: 20
   },
   image: {
     width: 200,
