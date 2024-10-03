5d0465933e1313a227b034df92e1eb176cb91c92
msundby <114078619+msundby@users.noreply.github.com>
2024-10-03 15:42:03 +0200

    Updates

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet,Image } from 'react-native';
 import Button from '../components/Button';
 
 
@@ -7,6 +7,10 @@ const Register: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sign up</Text>
+      
+      <View style={styles.backgroundContainer}>
+        <Image source={require('../../assets/AddProfileImageGold.png')} style={styles.addPhoto}/>
+      </View>
 
       <View style={styles.form}>
         <TextInput
@@ -34,12 +38,23 @@ const Register: React.FC = () => {
           style={styles.input}
           placeholderTextColor="white"
         />
-      </View>
-      <Button
-      title="Hellosan"
+        <View style={styles.button}>
+        <Button
+      title="Upload Drivers License"
       onPress={() => {
         console.log('Button pressed');
       }} />
+        <View style={styles.button}>
+        <Button
+      title="Register"
+      onPress={() => {
+        console.log('Button pressed');
+      }} />
+      </View>
+      
+      </View>
+      </View>
+      
     </View>
   );
 };
@@ -51,11 +66,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 50,
     backgroundColor: '#2c2c2e'
+    
+  },
+  backgroundContainer: {
+    width: '100%',
+    alignItems: 'center',
+    padding: '2%'
+  },
+  addPhoto: {
+    width: 100, 
+    height: 100, 
   },
   title: {
     fontSize: 24,
     marginBottom: 30,
     color: "white"
+  },
+  subtitle: {
+    fontSize: 14,
+    marginBottom: 30,
+    color: "white",
   },
   form: {
     width: '100%',
@@ -66,9 +96,12 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginTop: 10,
-    marginBottom: 1,
+    marginBottom: 2,
     backgroundColor: '#3a3a3c'
   },
+  button: {
+    marginTop: 20
+  }
 });
 
 export default Register;
